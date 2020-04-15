d78c295393bf1bb0342c3e1b33131f65e412b71f
nobu <nobutaka@gatech.edu>
2020-04-14 18:39:36 -0700

    donish

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from bottle import HTTPResponse
 
 @bottle.route("/")
 def index():
-    return "I'm a nasty snake!"
+    return "I'm nasty."
 
 
 @bottle.post("/ping")
@@ -27,7 +27,7 @@ def start():
     Your response will control how your snake is displayed on the board.
     """
     data = bottle.request.json
-    print(f"start_data:\n{json.dumps(data, indent=2)}")
+    #print(f"start_data:\n{json.dumps(data, indent=2)}")
 
     response = {"color": "#fc0313", "headType": "fang", "tailType": "bolt"}
     return HTTPResponse(
@@ -40,9 +40,9 @@ def start():
 @bottle.post("/move")
 def move():
     """
-    Called when the Battlesnake Engine needs to know your next move.
+    Called when the Battlesnake Engine needs to know your next my_move.
     The data parameter will contain information about the board.
-    Your response must include your move of up, down, left, or right.
+    Your response must include your my_move of up, down, left, or right.
     """
     def heuristic(start_node, goal_node):
         start_x = start_node[1]
@@ -65,7 +65,7 @@ def move():
 
         return heuristic_map
 
-    # moves
+    # my_moves
     delta = [[-1, 0],  # go up
              [0, -1],  # go left
              [1, 0],  # go down
@@ -83,23 +83,6 @@ def move():
     my_head_x = snakes[0]['body'][0]['x']
     my_head_y = snakes[0]['body'][0]['y']
 
-    #list of dicts of food locations
-    food = data['board']['food']
-    # list in order of nearest to furthest food
-    food_arr = []
-    get_food = False
-    if len(food)>0:
-        for z in range(len(food)):
-            food_dist = heuristic((my_head_y, my_head_x),
-                                  (food[z]['y'],food[z]['x']))
-            # dont' go for food further than width away
-            if food_dist > width/2:
-                continue
-            food_arr.append([food_dist, food[z]['y'], food[z]['x']])
-            get_food=True
-        food_arr = sorted(food_arr, key=lambda x: x[0])
-        print(f'nearest food {food_arr[0]}')
-
     # get list of snakes and make snakes_grid
     # my snake is 3 (head), 4 (body), opponents are 0,0 for smaller snake,
     # 5,6 for larger snake
@@ -110,53 +93,78 @@ def move():
     head_val = 5
     body_val = 6
 
+    #todo: debugging
+    which_move = ''
+    my_move = ''
+
     for j in range(len(snakes)):
         curr_snake = snakes[j]
-        #iterate to last since tail is gone next move
-        for k in range(0,len(curr_snake['body'])-1,1):
+        # iterate to last since tail is gone next my_move
+        for k in range(0, len(curr_snake['body']) - 1, 1):
             # if opponent bigger than or equal to me avoid
-            if j!=0 and len(curr_snake['body']) >= my_body_len:
+            if j != 0 and len(curr_snake['body']) >= my_body_len:
                 head_val = 5
                 body_val = 6
                 # if head
-                if k==0:
+                if k == 0:
                     snakes_grid[curr_snake['body'][k]['y'],
                                 curr_snake['body'][k]['x']] = head_val
-                    # mark the 4 connected
+                    # todo: pick one not mark the 4 connected
                     for i in range(len(delta)):
                         next_head_y = curr_snake['body'][k]['y'] \
-                                    + delta[i][0]
+                                      + delta[i][0]
                         next_head_x = curr_snake['body'][k]['x'] \
-                                    + delta[i][1]
+                                      + delta[i][1]
                         # if in bounds
                         if 0 <= next_head_y < snakes_grid.shape[0] \
                                 and 0 <= next_head_x < snakes.grid.shape[1]:
                             snakes_grid[next_head_y, next_head_x] = head_val
             # opponent's body
-            elif j!= 0:
+            elif j != 0:
                 snakes_grid[curr_snake['body'][k]['y'],
                             curr_snake['body'][k]['x']] = body_val
 
             # if opponent smaller than me, don't care so leave as 0
-            elif j!=0 and len(curr_snake['body']) < my_body_len:
+            elif j != 0 and len(curr_snake['body']) < my_body_len:
                 break
             # if it's my head
-            elif j==0 and k==0:
+            elif j == 0 and k == 0:
                 snakes_grid[curr_snake['body'][k]['y'],
-                        curr_snake['body'][k]['x']] = 3
+                            curr_snake['body'][k]['x']] = 3
             # my body, can't hit it anyways
-            elif j==0:
+            elif j == 0:
                 snakes_grid[curr_snake['body'][k]['y'],
-                        curr_snake['body'][k]['x']] = body_val
+                            curr_snake['body'][k]['x']] = body_val
 
         # set them to default larger or equal to me
         head_val = 5
         body_val = 6
+    #todo: hits own tail so include on snakes grid
+    snakes_grid[snakes[0]['body'][-1]['y'], snakes[0]['body'][-1]['y']] = 6
 
-    print(f"snakes_grid\n {snakes_grid}")
+    #list of dicts of food locations
+    food = data['board']['food']
+    # list in order of nearest to furthest food tuples (dist, y,x)
+    food_arr = []
+    get_food = False
+    #if there is food
+    if len(food)>0:
+        for z in range(len(food)):
+            food_dist = heuristic((my_head_y, my_head_x),
+                                  (food[z]['y'],food[z]['x']))
+            # dont' go for food further than width away
+            #todo: 0.7 is param
+            if food_dist > width*0.5:
+                continue
+            food_arr.append([food_dist, food[z]['y'], food[z]['x']])
+            get_food=True
+        food_arr = sorted(food_arr, key=lambda x: x[0])
+    #print(f'nearest food {food_arr[0]}')
+
+    #print(f"snakes_grid\n {snakes_grid}")
     cost = 1
-    first_move = 0
-    move = ''
+    #first_my_move = 0
+
     path_found=False
     # there is a food so A star for route to food using snake grid for g
     if get_food:
@@ -173,14 +181,14 @@ def move():
             g = 0 # each step is 1
             heuristic_map = make_heuristic_map((goal_y, goal_x),
                                                snakes_grid)
-            print(f'heuristics map\n{heuristic_map}')
+            #print(f'heuristics map\n{heuristic_map}')
             f = g + heuristic_map[my_head_y, my_head_x]
 
             open_arr = [[f,g,my_head_y, my_head_x]]
             found = False # set when search complete
             resign = False # set when can't expand
             count = 0
-            # only need to return the next move? or calculate entire path?
+            # only need to return the next my_move? or calculate entire path?
             while not found and not resign:
                 if len(open_arr) == 0:
                     resign = True
@@ -209,38 +217,42 @@ def move():
                                 # if unvisited and traversible
                                 if closed[new_y,new_x] ==0 and \
                                         snakes_grid[new_y,new_x]==0:
-                                    # todo: the first move is where we are going
-                                    if count == 1:
-                                        first_move = i
+                                    # todo: the first my_move is where we are going
+
                                     g2 = g + cost
                                     f2 = g2 + heuristic_map[new_y,new_x]
                                     open_arr.append([f2,g2,new_y,new_x])
                                     closed[new_y,new_x]=1
             # found goal or resigned
-            if not resign:
-                path_found = True
-                # find next move, how to get to any non -1 in expand
+            if found:
+
+                # find next my_move, how to get to any non -1 in expand
                 # but also 2 deep
                 for i in range(len(delta)):
                     next_y = my_head_y + delta[i][0]
                     next_x = my_head_x + delta[i][1]
-                    #if expand is "1" and in bounds
+                    #if expand is "pos" and in bounds
                     if 0 <= next_y < expand.shape[0] and \
                             0 <= next_x < expand.shape[1] and \
-                            expand[next_y, next_x]!=-1:
+                            expand[next_y, next_x]>0:
+                        curr_spot = expand[next_y, next_x]
                         for j in range(len(delta)):
                             n_next_y = next_y + delta[j][0]
                             n_next_x = next_x + delta[j][1]
                             if 0 <= n_next_y < expand.shape[0] and \
                                     0 <= n_next_x < expand.shape[1] and \
-                                    expand[n_next_y, n_next_x] != -1:
-                                print(f'expand\n {expand}')
-                                move = delta_name[i]
-                # found path so break
-                break
+                                    expand[n_next_y, n_next_x]>curr_spot:
+                                #print(f'expand\n {expand}')
+                                my_move = delta_name[i]
+                                path_found = True
+                                which_move = 'food'
+                                break
+                    # found path so break
+                    if path_found:
+                        break
     # if food A-star had no path or no food within reach to begin with
     # so chase tail
-    if not path_found or not get_food:
+    if not path_found:
         # chase tail if nothing in food_arr
         goal_y = snakes[0]['body'][-1]['y']
         goal_x = snakes[0]['body'][-1]['x']
@@ -253,14 +265,14 @@ def move():
         g = 0  # each step is 1
         heuristic_map = make_heuristic_map([goal_y, goal_x],
                                            snakes_grid)
-        print(f'heuristics_map\n{heuristic_map}')
+        #print(f'heuristics_map\n{heuristic_map}')
         f = g + heuristic_map[my_head_y, my_head_x]
 
         open_arr = [[f, g, my_head_y, my_head_x]]
         found = False  # set when search complete
         resign = False  # set when can't expand
         count = 0
-        # calculate entire path but only use first move
+        # calculate entire path but only use first my_move
         while not found and not resign:
             if len(open_arr) == 0:
                 resign = True
@@ -288,35 +300,40 @@ def move():
                             # if unvisited and traversible
                             if closed[new_y,new_x] == 0 and \
                                     snakes_grid[new_y,new_x] == 0:
-                                # todo: the first move is where we are going
-                                if count == 1:
-                                    first_move = i
+                                # todo: the first my_move is where we are going
+
                                 g2 = g + cost
                                 f2 = g2 + heuristic_map[new_y,new_x]
                                 open_arr.append([f2, g2, new_y, new_x])
                                 closed[new_y][new_x] = 1
         # found goal or resigned
-        if not resign:
-            path_found = True
-            # find next move, how to get to spot that's not -1 in expand
+        if found:
+
+            # find next my_move, how to get to spot that's not -1 in expand
             # but choose a spot that has continuation, not just an explored one
             for i in range(len(delta)):
                 next_y = my_head_y + delta[i][0]
                 next_x = my_head_x + delta[i][1]
                 if 0 <= next_y < expand.shape[0] and \
                         0 <= next_x < expand.shape[1] and \
-                        expand[next_y, next_x]!=-1:
+                        expand[next_y, next_x]>0:
+                    curr_spot = expand[next_y, next_x]
                     for j in range(len(delta)):
                         n_next_y = next_y + delta[j][0]
                         n_next_x = next_x + delta[j][1]
                         if 0 <= n_next_y < expand.shape[0] and \
                             0 <= n_next_x < expand.shape[1] and \
-                            expand[n_next_y, n_next_x] != -1:
-                            print(f'expand\n {expand}')
-                            move = delta_name[i]
+                            expand[n_next_y, n_next_x]>curr_spot:
+                            #print(f'expand\n {expand}')
+                            my_move = delta_name[i]
+                            path_found = True
+                            which_move = 'tail'
+                            break
+                if path_found:
+                    break
 
     # pretty print
-    #print(f"move_data:\n{json.dumps(data, indent=2)}")
+    #print(f"my_move_data:\n{json.dumps(data, indent=2)}")
 
     #chasing tail nor search for food worked so random?
     if not path_found:
@@ -324,18 +341,24 @@ def move():
             next_y = my_head_y + delta[t][0]
             next_x = my_head_x + delta[t][1]
             if 0 <= next_y < snakes_grid.shape[0] and \
-                    0 <= next_x < snakes_grid.shape[1]:
-                move = delta_name[t]
-    # Choose a random direction to move in
-    #directions = ["up", "down", "left", "right"]
+                    0 <= next_x < snakes_grid.shape[1] and \
+                    snakes_grid[next_y, next_x]==0:
+                my_move = delta_name[t]
+                which_move = 'last resort'
+                break
 
-    #move = random.choice(directions)
+    # Choose a random direction to my_move in
+    #if not path_found:
+     #   directions = ["up", "down", "left", "right"]
+      #  my_move = random.choice(directions)
+       # path_found=True
 
     # Shouts are messages sent to all the other snakes in the game.
     # Shouts are not displayed on the game board.
-    shout = "Kurae!"
-    #print(f'move: {move}')
-    response = {"move": move, "shout": shout}
+    shout = "tssss!"
+    turn = data['turn']
+    #print(f'\n\nturn: {turn}\nmy_move: {my_move}\n which_move: {which_move}\n\n')
+    response = {"move": my_move, "shout": shout}
     return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
@@ -370,7 +393,7 @@ def end():
     Called every time a game with your snake in it ends.
     """
     data = bottle.request.json
-    print(f"end data:\n{json.dumps(data, indent=2)}")
+    #print(f"end data:\n{json.dumps(data, indent=2)}")
     return HTTPResponse(status=200)
 
 
